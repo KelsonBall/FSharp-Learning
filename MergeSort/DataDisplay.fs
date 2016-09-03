50af76cfa038f858c42aeee9ce6e9e1717d67f69
Kelson Ball <kelson.j.ball@gmail.com>
2016-09-02 18:08:49 -0600

    Print test cases

---
+++ MergeSort/DataDisplay.fs
@@ -0,0 +1,12 @@
+﻿module DataDisplay =
+    let printArray (data:int[]) : unit =
+    let rec printElement (data:int[]) : unit =
+        if data.Length = 0 then
+            System.Console.WriteLine(System.Environment.NewLine)
+        else
+            System.Console.Write(", ")
+            System.Console.Write(data.[0])
+            printElement data.[1..]
+    System.Console.Write(data.[0])
+    printElement data.[1..]
+
--- MergeSort/Program.fs
+++ MergeSort/Program.fs
@@ -1,4 +1,7 @@
-﻿let rec merge (left:int[]) (right:int[]) : int[] =
+﻿// Merge Sort
+
+// Combine two sorted arrays into one sorted array
+let rec merge (left:int[]) (right:int[]) : int[] =
     if left.Length = 0 then
         right
     else if right.Length = 0 then
@@ -8,7 +11,7 @@
     else
         Array.concat[ [| right.[0] |] ; merge left right.[1..]]
 
-
+// Split array in half and then merge
 let rec mergesort fromIndex toIndex (data:int[]) : int[] =
     if fromIndex = toIndex then
         [| data.[fromIndex] |]
@@ -17,9 +20,14 @@ let rec mergesort fromIndex toIndex (data:int[]) : int[] =
         let right = mergesort ((toIndex + fromIndex) / 2 + 1) toIndex data
         merge left right
 
+// Logistics
+open DataDisplay
+
 [<EntryPoint>]
 let main argv =
     let rnd = System.Random()
-    let unsortedData = (Array.init (rnd.Next(5, 30)) (fun _ -> rnd.Next(100)))
-    let sortedData = mergesort 0 (unsortedData.Length - 1) unsortedData
+    for i = 0 to 10 do
+        let unsortedData = (Array.init (rnd.Next(5, 30)) (fun _ -> rnd.Next(100)))
+        let sortedData = mergesort 0 (unsortedData.Length - 1) unsortedData
+        DataDisplay.printArray sortedData
     0
